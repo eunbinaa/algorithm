1eba270fe0dcc41b796ef5158c3eb97ab52aeb40
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2023-09-13 13:49:22 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 208 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		double result=(2*a)+ (3.141592*b*2);
+		System.out.print(result);
+	}
+
+}
