9e2e6d09d3da4cf6f362739b4c48fd6440d1e76f
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2023-10-08 23:49:19 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,14 +1,16 @@
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0+1+2+3+4+5+6+7+8+9;
-        int[] num2={0,1,2,3,4,5,6,7,8,9};
+       /* int[] num2={0,1,2,3,4,5,6,7,8,9};
         for(int i=0; i<numbers.length; i++){
             for(int k=0; k<num2.length; k++){
                 if(numbers[i]==num2[k]){
                     answer-=numbers[i];
                 }
             }
-        }
+        }*/
+        for(int i : numbers)
+            answer-=i;
         return answer;
     }
 }
