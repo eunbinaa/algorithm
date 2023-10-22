50309fd42a32f8fa0e2cd66a111eb9881dc4d24f
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2023-10-22 15:21:41 +0900

    [level unrated] Title: 세균 증식, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -1,8 +1,11 @@
 import java.math.*;
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
-       answer= n*(int)Math.pow(2,t);
+        int answer = n;
+   //    answer= n*(int)Math.pow(2,t);
+        for(int i=1; i<=t; i++){
+            answer=answer*2;
+        }
         return answer;
     }
 }
