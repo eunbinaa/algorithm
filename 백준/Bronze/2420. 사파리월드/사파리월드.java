678bad9a2a5af1c3de21472162cb44f5bf8821b7
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2023-12-06 22:22:24 +0900

    [Bronze V] Title: 사파리월드, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+			
+			
+	       Scanner sc= new Scanner(System.in);
+	       long a=sc.nextInt();	       
+	       long b=sc.nextInt();
+	       
+	      System.out.println(Math.abs(a-b));
+	}
+
+}
+
