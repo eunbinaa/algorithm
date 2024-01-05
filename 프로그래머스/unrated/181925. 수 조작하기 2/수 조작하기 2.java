720eaea4b67d4642dcab30cfd95a16a7f32bbf95
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-01-05 15:33:06 +0900

    [level unrated] Title: 수 조작하기 2, Time: 634.22 ms, Memory: 377 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        for(int i=0; i<numLog.length-1;  i++){
+       /* for(int i=0; i<numLog.length-1;  i++){
             for(int j=i+1; j<=i+1; j++){               
                 if(numLog[i]+1==numLog[j]){
                     answer+="w";
@@ -12,6 +12,19 @@ class Solution {
                 }else if(numLog[i]-10==numLog[j]){
                     answer+="a";
                 }
+            }
+        }*/
+        for(int i=1; i<numLog.length; i++){
+            int j=numLog[i-1]-numLog[i];
+            switch(j){
+                case -1:
+                    answer+="w"; break;
+                case -10:
+                    answer+="d"; break;
+                case 10:
+                    answer+="a"; break;
+                case 1:
+                    answer+="s"; break;
             }
         }
         return answer;
