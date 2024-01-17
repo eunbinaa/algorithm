37d3c7bb24c0ef693bbfca002781401d8eabf558
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-01-17 20:20:25 +0900

    [level 2] Title: 전화번호 목록, Time: 232.56 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -8,12 +8,14 @@ class Solution {
             list.add(phone_book[i]);
         }
        
+      //  Collections.sort(list, (String a, String b) -> a.length() - b.length());  //길이 최선+크기 차선
+        // {12,123,1235,567,88} -> {12,88,123,567,1235} 순으로 정렬됨
          Collections.sort(list);  
-        for(int i=0; i<list.size()-1; i++){                         
-              if(list.get(i+1).startsWith(list.get(i))) {
-                  answer= false;      
-                  break;
-              }
+   
+       for(int i=0; i<list.size()-1; i++){   
+            if(list.get(i+1).startsWith(list.get(i))) {        
+                return false;
+                }
              }
      
         return answer;
