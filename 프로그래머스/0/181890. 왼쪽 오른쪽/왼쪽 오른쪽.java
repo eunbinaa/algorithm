156b99011b0be796ff3e4692bcb65a5b8c4f1d83
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-01-30 00:42:31 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -3,6 +3,9 @@ class Solution {
         String[] answer = {};
         int count=0;
         String[]str=new String[str_list.length];
+        //배열을 따로 하나 더 만드는 건 비효율적이라고 판단해서 str_list만 이용해서 풀려고 했는데
+        //테스트케이스에서 자꾸 몇개가 오류나서 결국 임시 배열 str을 생성했다.
+        //임시 배열에 일단 넣고 count크키 만큼 다시 answer에 저장하는 방식.
         for(int i=0; i<str_list.length; i++){
             if(str_list[i].equals("l")){
                
