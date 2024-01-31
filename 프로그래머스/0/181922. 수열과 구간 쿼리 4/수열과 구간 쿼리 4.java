448cfa52b65071e035b20bbbd14427d3151fbe0f
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-02-01 01:04:51 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 6.09 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        for(int i=0; i<queries.length; i++){
+            for(int j=0; j<arr.length; j++){
+                   if(j>=queries[i][0] && j<=queries[i][1]){
+                if(j%queries[i][2]==0){
+                    arr[j]+=1;
+                }
+            }     
+            }
+           
+        }
+        return arr;
+    }
+}
