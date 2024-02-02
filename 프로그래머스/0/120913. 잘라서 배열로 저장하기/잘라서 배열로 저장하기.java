b5a34f72cbb42099728ff3e641482821acefa441
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-02-02 17:51:03 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_str, int n) {
+        String[] answer = {};
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=0; i<my_str.length(); i+=n){
+            if(i+n>my_str.length()){
+                list.add(my_str.substring(i));
+            }else{
+                list.add(my_str.substring(i,i+n));
+            }          
+        }
+        answer=new String[list.size()];
+        for(int i=0; i<answer.length; i++) answer[i]=list.get(i);
+        return answer;
+    }
+}
