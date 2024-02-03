0e8ee45d2266909d542dd5351fe065745a94ab7a
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-02-03 22:27:32 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.19 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string = my_string.replaceAll("[^0-9]", " ");       
+        String[] str=my_string.split(" ");
+        
+      for(int i=0; i<str.length; i++){
+         if(!str[i].equals("")){
+             answer+=Integer.parseInt(str[i]);
+         }
+      }
+        
+        return answer;
+             
+ 
+    
+    
+    }    
+}
