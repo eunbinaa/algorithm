53fc4aa0df41d5764830d8e1ab05d626782e15a9
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-02-24 22:45:56 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        answer=new int[photo.length];
+        int sum=0;
+        int answer2=0;
+        for(int i=0; i<photo.length; i++){
+            for(int j=0; j<photo[i].length; j++){
+                for(int k=0; k<name.length; k++){
+                           if(photo[i][j].equals(name[k])){
+                            sum+=yearning[k];
+                        }
+                    }
+                
+            }
+            answer[i]=sum;
+            sum=0;
+        
+        }
+        return answer;
+    }
+}
