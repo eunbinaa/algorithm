d4ab15a17e8db1701124ffd3d844939257a1539d
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-03-25 14:53:59 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 1036 ms, Memory: 21124 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 int num= Integer.parseInt(br.readLine());
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<num*5; j++) {
+				 System.out.print("@");
+			 }
+			 System.out.println();
+		 }
+		 for(int i=0; i<3*num; i++) {
+			 for(int j=0; j<num; j++) {
+				 System.out.print("@");
+			 }
+			 System.out.println();
+		 }
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<num*5; j++) {
+				 System.out.print("@");
+			 }
+			 System.out.println();
+		 }
+	}
+}
