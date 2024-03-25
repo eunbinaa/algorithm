cdbca6dd910177dc00361acf7e1372108b1f85ed
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-03-25 15:05:13 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 140 ms, Memory: 15384 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		StringBuilder sb=new StringBuilder();
+		 int num= Integer.parseInt(br.readLine());
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<num*5; j++) {
+				 sb.append('@');
+			 }
+			   sb.append('\n');
+		 }
+		 for(int i=0; i<3*num; i++) {
+			 for(int j=0; j<5*num; j++) {
+				 if(j<num) {
+					 sb.append('@');
+				 }else if(j>(5*num)-num-1) {
+					 sb.append('@');
+				 }else {
+					 sb.append(" ");
+				 }
+			 }
+			 sb.append('\n');
+		 }
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<num*5; j++) {
+				 sb.append('@');
+			 }
+			 sb.append('\n');
+		 }
+		 System.out.println(sb.toString());
+	}
+}
