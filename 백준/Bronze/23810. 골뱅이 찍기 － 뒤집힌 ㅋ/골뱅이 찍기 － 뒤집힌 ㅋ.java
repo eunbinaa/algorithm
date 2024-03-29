a9560e6816bedd2c052dd2ae0d9b9884d9a9cf47
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-03-29 20:48:14 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 96 ms, Memory: 13288 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		StringBuilder sb=new StringBuilder();
+		 int num= Integer.parseInt(br.readLine());
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<num*5; j++) {
+				 sb.append('@');
+			 }
+			   sb.append('\n');
+		 }
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<num; j++) {				
+					 sb.append('@');				 
+			 }
+			 sb.append('\n');
+		 }
+		 
+		 
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<num*5; j++) {
+				 sb.append('@');
+			 }
+			   sb.append('\n');
+		 }
+		 for(int i=0; i<2*num; i++) {
+			 for(int j=0; j<num; j++) {				
+					 sb.append('@');				 
+			 }
+			 sb.append('\n');
+		 }
+	
+		 
+		 System.out.println(sb.toString());
+	}
+}
