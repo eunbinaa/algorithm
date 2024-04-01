713ff7240aaae6a269a70e0cbaa59e14562b0387
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-04-01 20:45:15 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 116 ms, Memory: 15052 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		StringBuilder sb=new StringBuilder();
+		 int num= Integer.parseInt(br.readLine());
+		
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<5*num; j++) {		
+				 if(j<num || j>5*num-num-1) {
+					 sb.append('@'); 
+				 }else{
+					 sb.append(' ');
+				 }
+			 }
+			 sb.append('\n');
+		 }
+	
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<4*num; j++) {		
+				 if(j<num || j>4*num-num-1) {
+					 sb.append('@'); 
+				 }else{
+					 sb.append(' ');
+				 }
+			 }
+			 sb.append('\n');
+		 }
+		 for(int i=0; i<num; i++) {
+		      for(int j=0; j<num*3; j++) {
+			 sb.append('@');
+		 }
+		   sb.append('\n');
+	     }
+	
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<4*num; j++) {		
+				 if(j<num || j>4*num-num-1) {
+					 sb.append('@'); 
+				 }else{
+					 sb.append(' ');
+				 }
+			 }
+			 sb.append('\n');
+		 }
+		 for(int i=0; i<num; i++) {
+			 for(int j=0; j<5*num; j++) {		
+				 if(j<num || j>5*num-num-1) {
+					 sb.append('@'); 
+				 }else{
+					 sb.append(' ');
+				 }
+			 }
+			 sb.append('\n');
+		 }
+		 System.out.println(sb.toString());
+	}
+}
