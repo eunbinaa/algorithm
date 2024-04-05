a80949e027f2d691536027ab2657dbcfa377f7ee
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-04-05 21:04:11 +0900

    [Bronze V] Title: Rook, Time: 76 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+
+				sb.append("  ___  ___  ___");
+				sb.append('\n');
+				sb.append("  | |__| |__| |");
+				sb.append('\n');
+				sb.append("  |           |");
+				sb.append('\n');
+				sb.append("   \\_________/");
+				sb.append('\n');
+				sb.append("    \\_______/");
+				sb.append('\n');
+				sb.append("     |     |");
+				sb.append('\n');
+				sb.append("     |     |");
+				sb.append('\n');
+				sb.append("     |     |");
+				sb.append('\n');
+				sb.append("     |     |");
+				sb.append('\n');
+				sb.append("     |_____|");
+				sb.append('\n');
+				sb.append("  __/       \\__");
+				sb.append('\n');
+				sb.append(" /             \\");				
+				sb.append('\n');
+				sb.append("/_______________\\");
+		 System.out.println(sb.toString());
+		
+		 
+		 
+	}
+}
