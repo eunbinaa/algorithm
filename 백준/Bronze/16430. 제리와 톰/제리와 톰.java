c46a1f77d654538f1426fb7746b19d295f26d9a2
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-04-17 22:06:24 +0900

    [Bronze V] Title: 제리와 톰, Time: 80 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		String str=br.readLine();
+		String[] a = str.split(" ");
+		
+		System.out.println(Integer.parseInt(a[1])-Integer.parseInt(a[0])+" "+Integer.parseInt(a[1]));
+	}
+}
