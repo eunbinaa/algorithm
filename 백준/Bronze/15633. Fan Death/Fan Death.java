1a553e833104d51e5b7796c0d75e2abb7044e6a3
Eunbi <119823066+eunbinaa@users.noreply.github.com>
2024-05-10 22:48:07 +0900

    [Bronze III] Title: Fan Death, Time: 80 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/15633. Fan Death/Fan Death.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int a=Integer.parseInt(br.readLine());
+	    int sum=0;
+	    for(int i=1; i<=a; i++) {
+	    	if(a%i==0) {
+	    		sum+=i;
+	    	}
+	    }
+	  
+		System.out.println(sum*5-24);
+	}
+}
